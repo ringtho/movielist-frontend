3757aebe977f8abc3faa9c748ebb5d8749fbc28b
Smith Ringtho <sringtho@gmail.com>
2023-10-14 09:53:12 +0300

    refactor: Updated the name to movies

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { getMovies } from '../../api'
+import Movie from '../../components/Movie/Movie'
+import './Movies.scss'
+
+const Movies = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const getMoviesData = async () => {
+      try {
+        const { data } = await getMovies()
+        console.log(data.movies)
+        setMovies(data.movies)
+      } catch(error){
+        console.log(error)
+      }
+    }
+    getMoviesData()
+  }, [])
+
+  return (
+    <section className="movies_container">
+      <h1 className='movies_title'>Movies</h1>
+      <div className='movies_wrapper'>
+        {movies.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Movies
